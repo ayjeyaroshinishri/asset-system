28ca13085d899592bb17ad1bb6a42e165321e11a
Varshini <yourgithubemail@gmail.com>
2026-04-09 21:48:24 +0530

    v2 - View Asset Feature

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 
+let assets = ["Laptop","Chair"];
+
 app.get("/", (req,res)=>{
-    res.send("Asset System V1 - Add Asset");
+    res.send("Asset System V2 - View Assets");
 });
 
-app.listen(3001, ()=>{
-    console.log("Running V1");
+app.get("/view",(req,res)=>{
+    res.json(assets);
+});
+
+app.listen(3002, ()=>{
+    console.log("Running V2");
 });
